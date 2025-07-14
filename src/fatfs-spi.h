8e113962403268412519fdb7ea109f229c641e75
Avamander <avamander@gmail.com>
2025-07-14 14:58:30 +0300

    Initial commit

---
+++ src/fatfs-spi.h
@@ -0,0 +1,12 @@
+#pragma once
+
+extern "C" {
+#include "ff14a/source/ff.h"
+#include "ff14a/source/diskio.h"
+#include "sd_driver/sd_card.h"
+#include "include/f_util.h"
+#include "include/ff_stdio.h"
+#include "include/my_debug.h"
+#include "include/rtc.h"
+#include "include/util.h"
+}
